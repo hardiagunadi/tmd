0cc82eae8ee6100f1e67243a78b7028a6eff617f
Hardi Agunadi <zigunx01@gmail.com>
2025-11-30 13:12:41 +0700

    Add authentication and credential management

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,32 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TagihanController;
+use Illuminate\Support\Facades\Route;
 
-// Arahkan halaman utama ke /tagihan
 Route::get('/', function () {
     return redirect()->route('tagihan.index');
 });
 
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'create'])->name('login');
+    Route::post('/login', [AuthController::class, 'store'])->name('login.store');
+});
 
-// GROUP ROUTE TAGIHAN
-Route::prefix('tagihan')->name('tagihan.')->group(function () {
-    Route::get('/', [TagihanController::class, 'index'])->name('index');
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'destroy'])->name('logout');
 
-    Route::get('/import', [TagihanController::class, 'importForm'])->name('import.form');
-    Route::post('/import/preview', [TagihanController::class, 'importPreview'])->name('import.preview');
-    Route::post('/import/store', [TagihanController::class, 'importStore'])->name('import.store');
+    Route::get('/profil', [AuthController::class, 'edit'])->name('credentials.edit');
+    Route::post('/profil', [AuthController::class, 'update'])->name('credentials.update');
 
-    Route::post('/print-batch', [TagihanController::class, 'printBatch'])->name('print.batch');
-    Route::get('/{tagihan}/print', [TagihanController::class, 'print'])->name('print');
+    Route::prefix('tagihan')->name('tagihan.')->group(function () {
+        Route::get('/', [TagihanController::class, 'index'])->name('index');
+
+        Route::get('/import', [TagihanController::class, 'importForm'])->name('import.form');
+        Route::post('/import/preview', [TagihanController::class, 'importPreview'])->name('import.preview');
+        Route::post('/import/store', [TagihanController::class, 'importStore'])->name('import.store');
+
+        Route::post('/print-batch', [TagihanController::class, 'printBatch'])->name('print.batch');
+        Route::get('/{tagihan}/print', [TagihanController::class, 'print'])->name('print');
+    });
 });
