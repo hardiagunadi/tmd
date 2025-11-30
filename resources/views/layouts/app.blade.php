c7e961c42cccc8ce8a655e28e602c65008ce5f5a
Hardi Agunadi <zigunx01@gmail.com>
2025-12-01 05:12:35 +0700

    Remove tagihan rekap view and route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,8 @@
         @auth
             <div class="navbar-nav me-auto">
                 <a class="nav-link" href="{{ route('tagihan.index') }}">Daftar Tagihan</a>
-                <a class="nav-link" href="{{ route('tagihan.rekap') }}">Rekap Penarikan</a>
                 <a class="nav-link" href="{{ route('tagihan.penarikan.index') }}">Entri Penarikan</a>
+                <a class="nav-link" href="{{ route('lainnya.index') }}">Pendapatan/Pengeluaran Lainnya</a>
             </div>
         @endauth
         <div class="d-flex align-items-center gap-2">
